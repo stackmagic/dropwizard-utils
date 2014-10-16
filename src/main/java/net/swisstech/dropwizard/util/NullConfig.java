0068dac49db723ceb93666eb9d49ef687b853726
Patrick Huber <stackmagic@gmail.com>
2014-10-16 18:38:33 +0200

    empty dw config

---
+++ src/main/java/net/swisstech/dropwizard/util/NullConfig.java
@@ -0,0 +1,6 @@
+package net.swisstech.dropwizard.util;
+
+import io.dropwizard.Configuration;
+
+/** just an empty config if your DW project happens to be config-less */
+public class NullConfig extends Configuration {}
