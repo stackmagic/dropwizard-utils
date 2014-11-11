8f7fd3742b992e0c2d38e594aea1d2ca8e2a44f6
Patrick Huber <stackmagic@gmail.com>
2014-11-11 12:32:37 +0100

    javadoc

--- src/main/java/net/swisstech/dropwizard/util/DummyResourceBundle.java
+++ src/main/java/net/swisstech/dropwizard/util/DummyResourceBundle.java
@@ -11,6 +11,10 @@ import javax.ws.rs.core.MediaType;
 
 import com.codahale.metrics.annotation.Timed;
 
+/**
+ * just a dummy resource which is also a bundle for easy registration - this is just a placeholder until you've added some real resources to your skeleton
+ * projects
+ */
 @Path("/net_swisstech_dropwizard_util_DummyResourceBundle")
 @Produces(MediaType.APPLICATION_JSON)
 public class DummyResourceBundle implements Bundle {
